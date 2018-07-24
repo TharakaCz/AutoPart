e14162bcf131c9531bd6a7a1c26bdc049694aedc
TharakaCz <tharakacz823@gmail.com>
2018-07-25 00:06:38 +0530

    Routing @Succsess

--- Auto-Part/FrontEnd/Users/src/app/app.module.ts
+++ Auto-Part/FrontEnd/Users/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { ViewComponent } from './view/view.component';
-import { MainComponent } from './view/main/main.component';
+
 import { GigPageComponent } from './view/gig-page/gig-page.component';
 import { DashbordComponent } from './view/dashbord/dashbord.component';
 import {RoutingModule} from "./routing/routing.module";
@@ -14,7 +14,6 @@ import {RoutingModule} from "./routing/routing.module";
   declarations: [
     AppComponent,
     ViewComponent,
-    MainComponent,
     GigPageComponent,
     DashbordComponent,
 
