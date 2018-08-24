df4adedfbd9a41d429fd8529e7b834286c3da882
TharakaCz <tharakacz823@gmail.com>
2018-08-24 22:31:22 +0530

    Main Proccess

--- Auto-Part/BackEnd/src/main/java/lk/ijse/autopart/rest/repository/OrderDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.autopart.rest.repository;
-
-import lk.ijse.autopart.rest.entity.OrderDelatil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDelatil,Integer> {
-}
--- Auto-Part/BackEnd/src/main/java/lk/ijse/autopart/rest/repository/OrdersRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.autopart.rest.repository;
-
-import lk.ijse.autopart.rest.entity.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepository extends JpaRepository<Orders,Integer> {
-}
